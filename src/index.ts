bd9a90c8be606e44afe92fdccd4930b8cf05b3d6
washington-shoji <shoji.washington@gmail.com>
2022-01-18 17:59:02 +1100

    refactor(core): Added error handling midwares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import * as dotenv from 'dotenv';
 import express from 'express';
+
+import * as dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+
+import {errorHandler} from './middleware/error.middleware';
+import {notFoundHandler} from './middleware/not-found.middleware';
 
 dotenv.config();
 
@@ -18,6 +22,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
