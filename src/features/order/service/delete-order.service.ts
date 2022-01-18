769ba916efde1ecbf352a36cabb1d80e190e8964
washington-shoji <shoji.washington@gmail.com>
2022-01-19 10:03:38 +1100

    feat(delete order service): Added the delete order service

---
+++ src/features/order/service/delete-order.service.ts
@@ -0,0 +1,15 @@
+import {collections} from '../../../services/db/mongo-database.service';
+import {DeleteResult, ObjectId} from 'mongodb';
+
+export async function deleteOrderService(id: string): Promise<void | null> {
+	try {
+		const query = {_id: new ObjectId(id)};
+		const deletedOrder: DeleteResult | undefined =
+			await collections.order?.deleteOne(query);
+		if (deletedOrder?.deletedCount === 0) {
+			throw new Error('could not find the order');
+		}
+	} catch (error: any) {
+		throw new Error(`Unable to delete order, due to ${error.message}`);
+	}
+}
