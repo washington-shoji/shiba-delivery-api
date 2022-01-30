c63c8feaad959d848657d6d724c52becb16c5f40
washington-shoji <shoji.washington@gmail.com>
2022-01-30 11:34:58 +1100

    refactor(middleware): Refactored the router to incorporate Auth0 authentication middleware

--- src/features/order/routes/order.routes.ts
+++ src/features/order/routes/order.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {jwtCheck} from '../../../middleware/authzero.middleware';
 import {createOrderController} from '../controllers/createOrderController.controller';
 import {deleteOrderController} from '../controllers/deleteOrderController.controller';
 import {getAllOrderController} from '../controllers/getAllOrderController.controller';
@@ -6,6 +7,10 @@ import {getSingleOrderController} from '../controllers/getSingleOrder.controller
 import {updateOrderController} from '../controllers/updateOrderController.controller';
 
 export const orderRouter = express.Router();
+
+// Mount authorization middleware
+
+orderRouter.use(jwtCheck);
 
 orderRouter.post('/order/', createOrderController);
 orderRouter.put('/order/:id', updateOrderController);
